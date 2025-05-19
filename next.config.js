03f79872eeab0e90cbcf4a98d53ee4ef2e4648e6
lakshitayadavv <168067588+lakshitayadavv@users.noreply.github.com>
2025-05-20 03:29:03 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
-// next.config.js
 const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
 });
 
-module.exports = withPWA({
-  // Your Next.js config here
-});
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // other config options if you have any
-}
+  // Add other custom config options if needed
+};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
+
 
